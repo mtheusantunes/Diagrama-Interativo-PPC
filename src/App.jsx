a01814a263d7291f12ca567931584b6c9fb9b5c1
matheusAntunesF <matheusantunesfreire.123@gmail.com>
2026-01-08 11:43:00 -0300

    build: project set up with Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Course from "./components/Course";
+
+function App() {
+  return (
+    <div>
+      <h1>Projeto Pedagógico de Curso (PPC) - Ciência da Computação</h1>
+      <Course/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Course.jsx
@@ -0,0 +1,7 @@
+function Course() {
+    return (
+        <h1>Disciplina</h1>
+    );
+}
+
+export default Course;
