b52ea4f84803001dce8fe12cb82ba8d85ae6462a
matheusAntunesF <matheusantunesfreire.123@gmail.com>
2026-01-08 21:05:52 -0300

    add: create Course component

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,7 +1,14 @@
 function Course() {
-    return (
-        <h1>Disciplina</h1>
-    );
+  return (
+    <div
+      className="w-[28vw] min-w-28 max-w-36 h-[21vw] min-h-24 max-h-28
+       md:w-[8.6vw] md:max-w-40 md:min-w-32 md:h-[6.5vw] md:max-h-32 md:min-h-24 bg-black 
+       text-white border-violet-600 rounded-xl border-4 flex items-center 
+       justify-center font-bold text-base md:text-sm text-center cursor-pointer select-none"
+    >
+      <p>Disciplina</p>
+    </div>
+  );
 }
 
 export default Course;
