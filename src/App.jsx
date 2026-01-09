913608ae5860582eec273579913b0f9af4427710
matheusAntunesF <matheusantunesfreire.123@gmail.com>
2026-01-08 22:56:35 -0300

    add: create and style basic App containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import Course from "./components/Course";
+import "./App.css";
+import Semester from "./components/Semester";
 
 function App() {
   return (
-    <div>
-      <h1>Projeto Pedagógico de Curso (PPC) - Ciência da Computação</h1>
-      <Course/>
+    <div className="w-screen h-screen bg-violet-900 flex flex-col justify-center p-6 overflow-x-hidden">
+      <h1 className="text-white text-center">
+        Projeto Pedagógico de Curso (PPC) - Ciência da Computação
+      </h1>
+      <div className="grid overflow-x-auto scrollbar-gutter-stable custom-scroll md:scrollbar-thin">
+        <div className="flex gap-4 my-2 mx-auto">
+          <Semester />
+          <Semester />
+          <Semester />
+          <Semester />
+          <Semester />
+          <Semester />
+          <Semester />
+          <Semester />
+          <Semester />
+          <Semester />
+        </div>
+      </div>
     </div>
   );
 }
