1a1eb2b105bcd38aa07ecc969a41c743e469f6f7
matheusAntunesF <matheusantunesfreire.123@gmail.com>
2026-01-10 14:40:14 -0300

    refactor: add a displayStatus to separate done/undone from derived attributes

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,25 +1,32 @@
 import { COURSE_STATUS } from "../constants/COURSE_STATUS";
 
-function Course(props) {
+function Course({ course, onCourseClick, isAvailable}) {
+  const isDone = course.status === COURSE_STATUS.DONE;
+  const isUnlocked = isAvailable(course);
+
+  let displayStatus = course.status;
+  if (!isDone) {
+    displayStatus = isUnlocked
+      ? COURSE_STATUS.AVAILABLE
+      :  COURSE_STATUS.UNAVAILABLE;
+  }
+
   return (
     <div
-      onClick={() => props.onCourseClick(props.course.id)}
+      onClick={() => onCourseClick(course.id)}
       className={`w-30 h-24 lg:w-32 text-white border-violet-600
          rounded-xl border-4 flex items-center justify-center font-bold text-xs
          text-center cursor-pointer select-none p-2 hyphens-auto 
          ${
-           props.course.status === COURSE_STATUS.AVAILABLE
+           displayStatus === COURSE_STATUS.AVAILABLE
              ? "bg-lime-500"
-             : "bg-black"
-         }
-         ${
-           props.course.status === COURSE_STATUS.DONE
+             : displayStatus === COURSE_STATUS.DONE
              ? "bg-sky-400"
              : "bg-black"
          }
          `}
     >
-      <p>{props.course.name}</p>
+      <p>{course.name}</p>
     </div>
   );
 }
