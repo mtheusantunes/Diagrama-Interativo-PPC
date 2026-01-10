c83123a6a0ef77ba4b4455eefac13c0c0227d6fe
matheusAntunesF <matheusantunesfreire.123@gmail.com>
2026-01-10 14:44:45 -0300

    refactor: destructure props in Semester component

--- src/components/Semester.jsx
+++ src/components/Semester.jsx
@@ -1,18 +1,22 @@
 import Course from "./Course";
 
-function Semester(props) {
-
-  const courses = props.semester.courses;
+function Semester({ semester, onCourseClick, isAvailable}) {
+  const courses = semester.courses;
   return (
     <div className="flex flex-col gap-4">
       <div
         className="flex justify-center bg-violet-950 border-4 rounded-xl border-violet-700 
          text-white font-bold text-base items-center h-12"
       >
-        <p>{props.semester.name}</p>
+        <p>{semester.name}</p>
       </div>
       {courses.map((course) => (
-        <Course key={course.id} course={course} onCourseClick={props.onCourseClick}/>
+        <Course
+          key={course.id}
+          course={course}
+          onCourseClick={onCourseClick}
+          isAvailable={isAvailable}
+        />
       ))}
     </div>
   );
