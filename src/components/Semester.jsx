4270947d69fc1657a6a1b519d5a85e307f567cac
matheusAntunesF <matheusantunesfreire.123@gmail.com>
2026-01-09 23:51:34 -0300

    feat: semester title block

--- src/components/Semester.jsx
+++ src/components/Semester.jsx
@@ -1,14 +1,22 @@
 import Course from "./Course";
 
 function Semester() {
-    return (
-        <div className="flex flex-col gap-4">
-            <Course/>
-            <Course/>
-            <Course/>
-            <Course/>
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-4">
+      <div
+        className="flex justify-center bg-violet-950 border-4 rounded-xl border-violet-700 
+         text-white font-bold text-base items-center h-12"
+      >
+        <p>1º Período</p>
+      </div>
+      <Course />
+      <Course />
+      <Course />
+      <Course />
+      <Course />
+      <Course />
+    </div>
+  );
 }
 
 export default Semester;
