0bd0dcfbd7fc6bced6d72f1ebc9741248377d765
matheusAntunesF <matheusantunesfreire.123@gmail.com>
2026-01-09 23:54:31 -0300

    feat: dinamically render courses in each semester

--- src/components/Semester.jsx
+++ src/components/Semester.jsx
@@ -1,20 +1,19 @@
 import Course from "./Course";
 
-function Semester() {
+function Semester(props) {
+
+  const courses = props.semester.courses;
   return (
     <div className="flex flex-col gap-4">
       <div
         className="flex justify-center bg-violet-950 border-4 rounded-xl border-violet-700 
          text-white font-bold text-base items-center h-12"
       >
-        <p>1º Período</p>
+        <p>{props.semester.name}</p>
       </div>
-      <Course />
-      <Course />
-      <Course />
-      <Course />
-      <Course />
-      <Course />
+      {courses.map((course) => (
+        <Course key={course.id} course={course} onCourseClick={props.onCourseClick}/>
+      ))}
     </div>
   );
 }
